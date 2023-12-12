3afc66ad6932114a2e980bde7ce2e0c630c0025f
derrod <xlnedder@gmail.com>
2023-12-13 00:34:35 +0100

    deps: Add stripped-down obs-deps build scripts

---
+++ deps/Build-Dependencies.ps1
@@ -0,0 +1,172 @@
+[CmdletBinding()]
+param(
+    [ValidateSet('Debug', 'RelWithDebInfo', 'Release', 'MinSizeRel')]
+    [string] $Configuration = 'Release',
+    [string[]] $Dependencies,
+    [ValidateSet('x64')]
+    [string] $Target,
+    [switch] $Clean,
+    [switch] $Quiet,
+    [switch] $Shared,
+    [switch] $SkipAll,
+    [switch] $SkipBuild,
+    [switch] $SkipDeps,
+    [switch] $SkipUnpack,
+    [switch] $VSPrerelease
+)
+
+$ErrorActionPreference = "Stop"
+
+if ( $DebugPreference -eq "Continue" ) {
+    $VerbosePreference = "Continue"
+    $InformationPreference = "Continue"
+}
+
+if ( $PSVersionTable.PSVersion -lt '7.2.0' ) {
+    Write-Warning 'The obs-deps PowerShell build script requires PowerShell Core 7.2+. Install or upgrade your PowerShell version: https://aka.ms/pscore6'
+    exit 0
+}
+
+function Run-Stages {
+    $Stages = @('Setup')
+
+    if ( ( $SkipAll ) -or ( $SkipBuild ) ) {
+        $Stages += @('Install', 'Fixup')
+    } else {
+        if ( $Clean ) {
+            $Stages += 'Clean'
+        }
+        $Stages += @(
+            'Patch'
+            'Configure'
+            'Build'
+            'Install'
+            'Fixup'
+        )
+    }
+
+    Log-Debug $DependencyFiles
+
+    $DependencyFiles | Sort | ForEach-Object {
+        $Dependency = $_
+
+        $Version = 0
+        $Uri = ''
+        $Hash = ''
+        $Path = ''
+        $Versions = @()
+        $Hashes = @()
+        $Uris = @()
+        $Patches = @()
+        $Options = @{}
+        $Targets = @($script:Target)
+
+        . $Dependency
+
+        if ( ! ( $Targets.contains($Target) ) ) {
+            $Stages | ForEach-Object {
+                Log-Debug "Removing function $_"
+                Remove-Item -ErrorAction 'SilentlyContinue' function:$_
+                $script:StageName = ''
+            }
+
+            return
+        }
+
+        if ( $Version -eq '' ) { $Version = $Versions[$Target] }
+        if ( $Uri -eq '' ) { $Uri = $Uris[$Target] }
+        if ( $Hash -eq '' ) { $Hash = $Hashes[$Target] }
+
+        if ( $Path -eq '' ) { $Path = [System.IO.Path]::GetFileNameWithoutExtension($Uri) }
+
+        Log-Output 'Initializing build'
+
+        $Stages | ForEach-Object {
+            $Stage = $_
+            $script:StageName = $Name
+            try {
+                Push-Location -Stack BuildTemp
+                if ( Test-Path function:$Stage ) {
+                    . $Stage
+                }
+            } catch {
+                Pop-Location -Stack BuildTemp
+                Log-Error "Error during build step ${Stage} - $_"
+            } finally {
+                $StageName = ''
+                Pop-Location -Stack BuildTemp
+            }
+        }
+
+        $Stages | ForEach-Object {
+            Log-Debug "Removing function $_"
+            Remove-Item -ErrorAction 'SilentlyContinue' function:$_
+            $script:StageName = ''
+        }
+
+        if ( Test-Path "$PSScriptRoot/licenses/${Name}" ) {
+            Log-Information "Install license files"
+
+            $null = New-Item -ItemType Directory -Path "$($ConfigData.OutputPath)/licenses" -ErrorAction SilentlyContinue
+            Copy-Item -Path "$PSScriptRoot/licenses/${Name}" -Recurse -Force -Destination "$($ConfigData.OutputPath)/licenses"
+        }
+    }
+}
+
+function Build-Main {
+    trap {
+        Write-Host '---------------------------------------------------------------------------------------------------'
+        Write-Host -NoNewLine '[OBS-UPDATER-DEPS] '
+        Write-Host -ForegroundColor Red 'Error(s) occurred:'
+        Write-Host '---------------------------------------------------------------------------------------------------'
+        Write-Error $_
+        exit 2
+    }
+
+    $script:PackageName = ((Get-Item $PSCommandPath).Basename).Split('-')[1]
+    if ( $script:PackageName -eq 'Dependencies' ) {
+        $script:PackageName = 'deps'
+    }
+
+    $UtilityFunctions = Get-ChildItem -Path $PSScriptRoot/utils.pwsh/*.ps1 -Recurse
+
+    foreach($Utility in $UtilityFunctions) {
+        Write-Debug "Loading $($Utility.FullName)"
+        . $Utility.FullName
+    }
+
+    Bootstrap
+
+    $SubDir = 'deps.windows'
+
+    if ( $Dependencies.Count -eq 0 ) {
+        $DependencyFiles = Get-ChildItem -Path $PSScriptRoot/${SubDir}/*.ps1 -File -Recurse
+    } else {
+        $DependencyFiles = $Dependencies | ForEach-Object {
+            $Item = $_
+            try {
+                Get-ChildItem $PSScriptRoot/${SubDir}/*$Item.ps1
+            } catch {
+                throw "Script for requested dependency ${Item} not found"
+            }
+        }
+    }
+
+    Log-Debug "Using found dependency scripts:`n${DependencyFiles}"
+
+    Push-Location -Stack BuildTemp
+
+    Ensure-Location $WorkRoot
+
+    Run-Stages
+
+    Pop-Location -Stack BuildTemp
+
+    Write-Host '---------------------------------------------------------------------------------------------------'
+    Write-Host -NoNewLine '[OBS-UPDATER-DEPS] '
+    Write-Host -ForegroundColor Green 'All done'
+    Write-Host "Built Dependencies: $(if ( $Dependencies -eq $null ) { 'All' } else { $Dependencies })"
+    Write-Host '---------------------------------------------------------------------------------------------------'
+}
+
+Build-Main
---
+++ deps/README.md
@@ -0,0 +1,5 @@
+# deps
+
+This is a stripped down version of the [obs-deps](https://github.com/obsproject/obs-deps) build scripts specifically for this repo.
+
+It builds only the dependencies required by the updater and puts them in a predefined folder in the project root which then gets picked up by cmake.
